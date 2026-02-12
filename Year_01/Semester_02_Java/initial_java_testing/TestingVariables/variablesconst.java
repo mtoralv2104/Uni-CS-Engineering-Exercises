282e38b342af72a11b05e5a9d81511a3ab002c21
Marcos Torres Álvarez <211049417+mtoralv@users.noreply.github.com>
2026-02-12 21:01:53 +0100

    Done Variables and constants exercises

---
+++ Year_01/Semester_02_Java/initial_java_testing/TestingVariables/variablesconst.java
@@ -0,0 +1,37 @@
+
+public class variablesconst
+{
+    public static void main(String[] args) {
+
+        // IMPORTANT String with UpperCase
+
+        String TestingStr = "This is a test";
+        System.out.println(TestingStr);
+        System.out.println(TestingStr);
+
+        int age = 8;
+        System.out.println(age);
+
+        final String email = "hello@uma.es";
+
+        System.out.println(email);
+
+        // var for automatic data type
+
+        var year = 2077;
+
+        System.out.println(year);
+
+        int a=1;
+        int b=2;
+
+        System.out.println(a+b);
+
+        // Testing cin syntax
+
+        
+        
+
+
+ }
+}
