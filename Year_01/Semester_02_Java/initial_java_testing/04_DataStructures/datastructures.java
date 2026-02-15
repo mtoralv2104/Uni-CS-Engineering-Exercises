d9e199dc5a6f0171af0bc56dbfecb54297be675e
Marcos Torres Álvarez <211049417+mtoralv@users.noreply.github.com>
2026-02-15 23:59:14 +0100

    Continued with data structures exercises (ArrayLists continuation) [UNFINISHED]

--- Year_01/Semester_02_Java/initial_java_testing/04_DataStructures/datastructures.java
+++ Year_01/Semester_02_Java/initial_java_testing/04_DataStructures/datastructures.java
@@ -21,8 +21,27 @@ public class datastructures {
         ArrayList<String> namesList = new ArrayList<>();
         var numbers = new ArrayList<Integer>();
 
+        namesList.add("Hamilton");
+        namesList.add("Starman");
+
+        numbers.add(3);
+
         System.out.println(namesList);
+        System.out.println(namesList.get(1));
+
+        // set an index position
+        namesList.set(1, "Bowie");
+        System.out.println(namesList.get(1));
+        namesList.remove(0);
+        System.out.println(namesList);
+
+        //contains
+        System.out.println(namesList.contains("Bowie"));
+
+
         System.out.println(numbers);
+
+
 
     }    
 }
